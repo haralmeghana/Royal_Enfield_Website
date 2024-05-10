b3e05d8d0c805debe3e9dd347a1e8e2c7d1fe181
haralmeghana <128888658+haralmeghana@users.noreply.github.com>
2024-05-10 20:20:36 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,85 @@
+function a(){
+    document.getElementById("abc1").src="./Images/14.avif"
+}
+function b(){
+    document.getElementById("abc1").src="./Images/15.avif"
+}
+function c(){
+    document.getElementById("abc1").src="./Images/16.avif"
+}
+function d(){
+    document.getElementById("abc1").src="./Images/17.avif"
+}
+function e(){
+    document.getElementById("abc1").src="./Images/18.avif"
+}
+function f(){
+    document.getElementById("abc1").src="./Images/19.avif"
+}
+function g(){
+    document.getElementById("abc1").src="./Images/20.avif"
+}
+function h(){
+    document.getElementById("abc1").src="./Images/21.avif"
+}
+function i(){
+    document.getElementById("abc1").src="./Images/8.avif"
+}
+function j(){
+    document.getElementById("abc2").src="./Images/22.avif"
+}
+function k(){
+    document.getElementById("abc2").src="./Images/23.avif"
+}
+function l(){
+    document.getElementById("abc2").src="./Images/9.avif"
+}
+
+function ll(){
+    document.getElementById("abc2").src="./Images/9.1.avif"
+}
+function mm(){
+    document.getElementById("abc2").src="./Images/9.2.avif"
+}
+function nn(){
+    document.getElementById("abc2").src="./Images/9.3.avif"
+}
+function oo(){
+    document.getElementById("abc2").src="./Images/9.4.avif"
+}
+function pp(){
+    document.getElementById("abc2").src="./Images/9.5.avif"
+}
+
+function n(){
+    document.getElementById("abc3").src="./Images/10.avif"
+}
+function o(){
+    document.getElementById("abc3").src="./Images/25.avif"
+}
+function q(){
+    document.getElementById("abc3").src="./Images/12.avif"
+}
+function m(){
+    document.getElementById("abc3").src="./Images/24.avif"
+}
+function u(){
+    document.getElementById("abc4").src="./Images/31.avif"
+}
+function r(){
+    document.getElementById("abc5").src="./Images/11.avif"
+}
+function t(){
+    document.getElementById("abc5").src="./Images/12.2.avif"
+}
+function v(){
+    document.getElementById("abc5").src="./Images/32.avif"
+}
+function w(){
+    document.getElementById("abc5").src="./Images/33.avif"
+}
+function x(){
+    document.getElementById("abc5").src="./Images/12.1.avif"
+}
+
+
